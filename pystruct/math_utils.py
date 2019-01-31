5407ee38b6ac5bbc356b78981e706ca5e291cd36
Aaron Moore <aaron@atamisk.net>
2019-01-31 06:05:37 -0500

    Revised beta function.

--- pystruct/math_utils.py
+++ pystruct/math_utils.py
@@ -1,4 +1,4 @@
 def calc_beta(stress, strength):
     top = strength.mu - stress.mu
-    bottom = (stress.sigma + strength.sigma)**0.5
+    bottom = (stress.sigma**2 + strength.sigma**2)**0.5
     return top/bottom
