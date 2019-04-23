b246bce421a28308198abc758a2ff965a7405590
Aaron Moore <aaron@atamisk.net>
2019-04-22 21:37:19 -0400

    Updated utility to grab beta too.

--- csv_fitness.py
+++ csv_fitness.py
@@ -1,4 +1,5 @@
 from pystruct.optim import *
+from pystruct import test_open_force_pack
 from pystruct import cost_stress
 from copy import deepcopy
 import argparse as arg
@@ -7,6 +8,7 @@ import csv
 def parseargs():
     parser = arg.ArgumentParser()
     parser.add_argument('fname',help="Filename for CSV file.")
+    parser.add_argument('--beta', help="Get Beta instead of peak stress", action='store_true')
     return parser.parse_args()
 
 def get_props_array(fname, base_props):
@@ -27,8 +29,20 @@ def get_props_array(fname, base_props):
 
 args = parseargs()
 fname = args.fname
-sys = system(0,'/tmp/test_open.dat',10,10,[cost_stress],[])
+if (args.beta == True):
+    x_force = test_open_force_pack(1,1000,0,0)
+    y_force = test_open_force_pack(1,0,1000,0)
+    sto_force_x = nr_var(0,5000)
+    sto_force_y = nr_var(150000,19500)
+    sys = system_unit(1,'/tmp/test_open.dat', 1,3, x_force, y_force, sto_force_x, sto_force_y)
+    fit_index = 1
+    fit_sign = -1
+else:
+    sys = system(0,'/tmp/test_open.dat',10,10,[cost_stress],[])
+    fit_index = 0
+    fit_sign = 1
+print(sys.base_props)
 props_array = get_props_array(fname, sys.base_props)
 fit = sys.run_generation(deepcopy,props_array)
 for x in fit:
-    print(x.fitness[0])
+    print(fit_sign * x.fitness[fit_index])
